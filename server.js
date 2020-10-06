95f1b74b13b5d7498a97cbbbc713931cf8786130
Erick Pacheco <linarespachecoerick@gmail.com>
2020-10-06 16:53:24 -0700

    Updates content of pages

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-const express  = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
 
-app.use(express.static())
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT);
+
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname + '/index.html')
+    
+))
